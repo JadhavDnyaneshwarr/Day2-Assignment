46be6b48ca68e02a3a5e67e5a57673f4f1f497cf
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-01-07 18:16:25 +0530

    If- Else Statement.

--- src/com/bridgelabz/assignment/AssignmentQue1.java
+++ src/com/bridgelabz/assignment/AssignmentQue1.java
@@ -1,7 +1,35 @@
 package com.bridgelabz.assignment;
 
+import java.util.Scanner;
+
 public class AssignmentQue1 {
     public static void main(String[] args) {
         System.out.println("welcome to Day 2 assignment");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the number => ");
+        int num = scanner.nextInt();
+        if (num==1){
+            System.out.println("the enter number is One.");
+        }else if (num==2) {
+            System.out.println("the enter number is Two.");
+        }else if (num==3) {
+            System.out.println("the enter number is Three.");
+        }else if (num==4) {
+            System.out.println("the enter number is Four.");
+        }else if (num==5) {
+            System.out.println("the enter number is Five.");
+        }else if (num==6) {
+            System.out.println("the enter number is Six.");
+        }else if (num==7) {
+            System.out.println("the enter number is Seven.");
+        }else if (num==8) {
+            System.out.println("the enter number is Nine.");
+        }else if (num==9) {
+            System.out.println("the enter number is Ten.");
+        }else if (num==0) {
+            System.out.println("the enter number is Zero.");
+        }else {
+            System.out.println("the enter number is Invalid.");
+        }
     }
 }
---
+++ src/com/bridgelabz/assignment/AssignmentQue1_2.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.assignment;
+
+import java.util.Scanner;
+
+public class AssignmentQue1_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the number(1,10,100,1000,100000,10000000 => ");
+        int num = scanner.nextInt();
+
+        if (num==1){
+            System.out.println("the Enter number is Unit");
+        }
+        else if (num==10){
+            System.out.println("the Enter number is Ten");
+        }
+        else if (num==100){
+            System.out.println("the Enter number is hundred");
+        }
+        else if (num==1000){
+            System.out.println("the Enter number is Thousand");
+        }
+        else if (num==100000){
+            System.out.println("the Enter number is Lakh");
+        }
+        else if (num==10000000){
+            System.out.println("the Enter number is Crore");
+        }
+        else {
+            System.out.println("the Enter number is Invalid");
+        }
+    }
+}
