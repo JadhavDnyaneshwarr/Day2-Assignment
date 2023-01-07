7e6e0502406e3c16b52d50f32fcd0b4640d56282
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-01-07 17:50:03 +0530

    welcome to assignment day2.

---
+++ src/com/bridgelabz/assignment/AssignmentQue1.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.assignment;
+
+public class AssignmentQue1 {
+    public static void main(String[] args) {
+        System.out.println("welcome to Day 2 assignment");
+    }
+}
