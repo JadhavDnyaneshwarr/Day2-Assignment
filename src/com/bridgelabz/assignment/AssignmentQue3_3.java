9c75a881c460109d77de8bbfc3fc64eb21caadb5
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-01-07 19:32:39 +0530

    For loop programs.

---
+++ src/com/bridgelabz/assignment/AssignmentQue3_3.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.assignment;
+
+import java.util.Scanner;
+
+public class AssignmentQue3_3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the number => ");
+        int num = scanner.nextInt();
+        int number = num;
+        int rev = 0;
+        int qu = 0;
+        for (; number!=0; number = number/10) {
+            qu = number % 10;
+            rev = rev * 10 + qu;
+        }
+        if (num==rev){
+            System.out.println("the number " + num + " is palindrome number");
+        }else {
+            System.out.println("the number " + num + " is not palindrome number");
+        }
+    }
+}
